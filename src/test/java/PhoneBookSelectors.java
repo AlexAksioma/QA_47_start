513f03a4c38f0876d28ccb7e754e9ac346b76d32
AlexAksioma <alexmedqwerty@gmail.com>
2025-05-22 09:22:17 +0300

    xPath selectors

--- src/test/java/PhoneBookSelectors.java
+++ src/test/java/PhoneBookSelectors.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -9,12 +11,32 @@ public class PhoneBookSelectors {
     WebDriver driver = new ChromeDriver();
 
     @Test
-    public void phoneBookTest(){
+    public void phoneBookTest() {
         driver.get("https://telranedu.web.app/home");
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //home work
         FirstTest.pause(5);
+        WebElement btnHome = driver.findElement(By.cssSelector("a[aria-current='page']"));
+        btnHome.click();
+        WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+        btnAbout.click();
+        WebElement div = driver.findElement(By.cssSelector("div[class='about_main__2Uv5W']"));
+        String divText = div.getText();
+        System.out.println(divText);
+        WebElement h1 = driver.findElement(By.cssSelector("div[class='about_main__2Uv5W']>h1"));
+        System.out.println("h1--> " + h1.getText());
+        FirstTest.pause(2);
+        WebElement btnLogin = driver.findElement(By.cssSelector("div[class='navbar-component_nav__1X_4m'] a:nth-child(4)"));
+        btnLogin.click();
+        WebElement inputEmail = driver.findElement(By.cssSelector("*[name='email']"));
+        inputEmail.sendKeys("email@mail.com");
+        WebElement inputPassword = driver.findElement(By.cssSelector("input[placeholder='Password']"));
+        inputPassword.sendKeys("Password");
+        WebElement btnLoginForm = driver.findElement(By.cssSelector("button[name='login']"));
+        btnLoginForm.click();
+        FirstTest.pause(5);
+        driver.quit();
     }
 }
